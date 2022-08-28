a33b0fd3ddf884ab2e45e06f606903a2df23a55d
GeoDimi99 <dimitrov.georgi.t@gmail.com>
2022-08-28 08:39:38 +0200

    SMART Lab voice control Algorithm

--- voice_control_turtlesim/scripts/voice_control_turtlesim.py
+++ voice_control_turtlesim/scripts/voice_control_turtlesim.py
@@ -4,19 +4,21 @@ import rospy
 from std_msgs.msg import String        #Importo questo tipo di messaggio per leggere il risultato finale su Vosk
 from geometry_msgs.msg import Twist		   #Importo questo tipo di messaggio per inviare i comandi alla turtlesim
 
-# Variabili globali
+import time
 
+# Variabili globali
+speed = 0.2
 
 
 def speechCallback(speech_msg):
-	#Dicchiarazione di variabili globali
-	global pub_vel
+	# Dicchiarazione di variabili globali
+	global pub_vel, speed
 	
-	#Lettura della parola riconosciuta
+	# Lettura della parola riconosciuta
 	rospy.loginfo(speech_msg.data)
 	spmsg = speech_msg.data
 	
-	#Creazione del messaggio e inizializzazione di tutti i campi a zero
+	# Creazione del messaggio e inizializzazione di tutti i campi a zero
 	twist_msg = Twist()
 	twist_msg.linear.x = 0.0
 	twist_msg.linear.y = 0.0
@@ -24,6 +26,55 @@ def speechCallback(speech_msg):
 	twist_msg.angular.x = 0.0
 	twist_msg.angular.y = 0.0
 	twist_msg.angular.z = 0.0
+	
+	# Controllo di quale comando è richiesto:
+	
+	# - Controllo velocità
+	if spmsg.find("full speed") > -1:
+		if speed == 0.2:
+			twist_msg.linear.x *= 2
+			twist_msg.angular.z *= 2
+			speed = 0.4
+	if spmsg.find("half speed") > -1:
+		if speed == 0.4:
+			twist_msg.linear.x /= 2
+			twist_msg.angular.z /= 2
+			speed = 0.4
+			
+	if spmsg.find("forward") > -1:
+		twist_msg.linear.x = speed
+		twist_msg.angular.z = 0
+	elif spmsg.find("left") > -1:
+		twist_msg.linear.x = 0
+		twist_msg.angular.z = speed*2
+	elif spmsg.find("right") > -1:
+		twist_msg.linear.x = 0
+		twist_msg.angular.z = -speed*2
+	elif spmsg.find("back") > -1:
+		twist_msg.linear.x = -speed
+		twist_msg.angular.z = 0
+	elif spmsg.find("move one meter") > -1:
+		t_end = time.time() + 5
+		while time.time() < t_end:
+			twist_msg.linear.x = speed
+			twist_msg.angular.z = 0
+			pub_vel.publish(twist_msg)
+	elif spmsg.find("move two meters") > -1:
+		t_end = time.time() + 10
+		while time.time() < t_end:
+			twist_msg.linear.x = speed
+			twist_msg.angular.z = 0
+			pub_vel.publish(self.twist_msg)
+	elif spmsg.find("move three meters") > -1:
+		t_end = time.time() + 15
+		while time.time() < t_end:
+			twist_msg.linear.x = self.speed
+			twist_msg.angular.z = 0
+			pub_vel.publish(self.twist_msg)
+	
+	elif spmsg.find("stop") > -1 or spmsg.find("halt") > -1:
+		twist_msg = Twist()
+		
 	
 	#Publicazione del messaggio
 	pub_vel.publish(twist_msg)
